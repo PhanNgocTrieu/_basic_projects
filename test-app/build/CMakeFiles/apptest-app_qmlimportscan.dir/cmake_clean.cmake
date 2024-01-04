3ec816d9b91c298f08e5ee03593a0947ec03668f
trieupn <pntrieu200799@gmail.com>
2024-01-04 07:25:10 +0700

    Update files for serve God

--- test-app/build/CMakeFiles/apptest-app_qmlimportscan.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  ".qt_plugins/Qt6_QmlPlugins_Imports_apptest-app.cmake"
-  "CMakeFiles/apptest-app_qmlimportscan"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/apptest-app_qmlimportscan.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- test-app/build/CMakeFiles/apptest-app_tooling.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "test-app/main.qml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/apptest-app_tooling.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
